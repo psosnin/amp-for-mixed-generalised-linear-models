ffdc3ba9b2a9102d0e2c0588d2eeffb43fb64a64
psosnin <philipsosnin@gmail.com>
2023-05-17 20:49:14 +0100

    Comment and format gamp code

--- gamp/fitting/gamp.py
+++ gamp/fitting/gamp.py
@@ -1,20 +1,20 @@
-from math import sqrt
-
 import numpy as np
-from numpy.random import default_rng
-
-RNG = default_rng(1)
 
 
 def GAMP(X, y, beta_hat_k, sigma_beta_sq, sigma_sq, n_iters, gk_expect):
     """
     Run generalised approximate message passing to estimate beta from X and y.
-    We assume a zero mean gaussian prior on the signal beta.
+    The model makes the following assumptions:
+        1. y is generated from a generalised linear model, y = q(X @ beta) + noise, where q is a link function.
+        2. The signal beta is drawn iid from a zero mean gaussian distribution with covariance sigma_beta_sq.
+        3. The covariates X are drawn iid from a zero mean gaussian distribution with covariance 1 / n.
+        4. The noise is drawn iid from a zero mean gaussian distribution with variance sigma_sq.
+        5. Initial estimate of beta is drawn from the true prior distribution of beta.
     Parameters:
-        X: n x p = samples
-        y: n x 1 = observations
-        beta_hat_k: p x 1 = initial estimate of beta
-        sigma_beta_sq: float = signal variance
+        X: n x p = covariate matrix
+        y: n = observations
+        beta_hat_k: p x 1 = initial estimate of signal beta
+        sigma_beta_sq: float = prior variance of beta
         sigma_sq: float = noise variance
         n_iters: int = max number of AMP iterations to perform
         gk_expect: function to compute E[Z | Zk, Ybar], depends on choice of GLM
@@ -22,48 +22,46 @@ def GAMP(X, y, beta_hat_k, sigma_beta_sq, sigma_sq, n_iters, gk_expect):
         beta_hat_list: list of estimates of B for each AMP iteration
         mu_k_list: list of state evolution mean for each iteration
     """
-    # validate input
+    # get dimensions and validate Y
     n, p = X.shape
     delta = n / p
-
-    # initialisation
-    r_hat_k_minus_1 = np.zeros(n)
-    b_k = 1
-    # assumes beta_hat_0 generated from the same distribution as beta:
-    Sigma_k = sigma_beta_sq * np.eye(2) / delta
-
-    # containers to store results
+    assert (y.shape == (n, ))
+    # initialise lists to store results
     beta_hat_list = [beta_hat_k]
     mu_k_list = []
+    # initialise variables
+    r_hat_k_minus_1 = np.zeros(n)
+    b_k = 1
+    Sigma_k = sigma_beta_sq * np.eye(2) / delta
 
     # begin AMP iterations
     for _ in range(n_iters):
-        # step 1:
+        # step 1: compute theta_k
         theta_k = X @ beta_hat_k - b_k * r_hat_k_minus_1
-        # step 2:
+        # step 2: apply denoising function gk and check for nan
         r_hat_k = apply_gk(theta_k, y, Sigma_k, sigma_sq, gk_expect)
         if np.isnan(r_hat_k).any():
             print(f"nan in r_hat_k at iteration {_}, stopping.")
             break
-        # state evolution:
+        # compute state evolution
         mu_k_plus_1 = sigma_k_plus_1_sq = r_hat_k.T @ r_hat_k / n
         q = mu_k_plus_1 * sigma_beta_sq / (mu_k_plus_1 ** 2 * sigma_beta_sq + sigma_k_plus_1_sq)
-        # step 3:
+        # step 3: compute c_k
         c_k = compute_ck(theta_k, r_hat_k, Sigma_k, mu_k_plus_1)
-        # step 4:
+        # step 4: compute beta_k estimate of beta
         beta_k_plus_1 = X.T @ r_hat_k - c_k * beta_hat_k
-        # step 5:
+        # step 5: apply denoising function fk assuming Gaussian prior
         beta_hat_k_plus_1 = q * beta_k_plus_1
-        # step 6:
+        # step 6: compute b_k
         b_k_plus_1 = q / delta
-        # update_sigma
+        # update state evolution matrix
         Sigma_k_plus_1 = update_Sigmak(q, mu_k_plus_1, sigma_beta_sq, delta)
-
         # prepare next iteration:
         beta_hat_k = beta_hat_k_plus_1
         Sigma_k = Sigma_k_plus_1
         r_hat_k_minus_1 = r_hat_k
         b_k = b_k_plus_1
+        # store estimates
         beta_hat_list.append(beta_hat_k)
         mu_k_list.append(mu_k_plus_1)
 
@@ -92,7 +90,4 @@ def compute_ck(theta_k, r_hat_k, Sigma_k, mu_k_plus_1):
 def update_Sigmak(q, mu_k_plus_1, sigma_beta_sq, delta):
     """ Update the state evolution. """
     S_12 = q * mu_k_plus_1 * sigma_beta_sq
-    return np.block([
-        [sigma_beta_sq, S_12],
-        [S_12, S_12]
-    ]) / delta
+    return np.block([[sigma_beta_sq, S_12], [S_12, S_12]]) / delta
--- gamp/fitting/matrix_gamp.py
+++ gamp/fitting/matrix_gamp.py
@@ -1,17 +1,24 @@
 import numpy as np
-from tqdm import tqdm
-from numpy.linalg import inv, pinv, norm
+from numpy.linalg import pinv, norm
 
 
 def matrix_GAMP(X, Y, B_hat_k, B_row_cov, sigma_sq, alpha, n_iters, apply_gk, eps=1e-2):
     """
-    Run matrix generalised approximate message passing to estimate B from X and Y.
+    Run matrix generalised approximate message passing to estimate B from X and Y
+    with a given initial estimate B_hat_k.
+    The model makes the following assumptions:
+        1. Y is generated from a mixed generalised linear model, Y = q(X @ B) + noise, where q is a link function.
+        2. The rows of B are drawn iid from a zero mean gaussian distribution with covariance B_row_cov.
+        3. The covariates X are drawn iid from a zero mean gaussian distribution with covariance 1 / n.
+        4. The noise is drawn iid from a zero mean gaussian distribution with variance sigma_sq.
+        5. Y_i is generated from a single column of B according to a categorical distribution with probabilities alpha.
+        6. Initial estimate of B is drawn from the true prior distribution of B.
     Parameters:
-        X: n x p = samples
+        X: n x p = covariate matrix
         Y: n x 1 = observations
-        B_hat_k: p x L = initial estimate of signal matrix
-        B_row_cov: L x L = covariance of distribution of the rows of B
-        sigma: int = noise variance
+        B_hat_k: p x L = initial estimate of signal matrix B
+        B_row_cov: L x L = prior covariance of distribution of the rows of B
+        sigma: float = noise variance
         alpha: L x 1 = categorical distribution on components
         n_iters: int = max number of AMP iterations to perform
         gk_expect: function to compute E[Z | Zk, Ybar], depends on choice of GLM
@@ -20,54 +27,50 @@ def matrix_GAMP(X, Y, B_hat_k, B_row_cov, sigma_sq, alpha, n_iters, apply_gk, ep
         B_hat_list: list of estimates of B for each AMP iteration
         M_k_B_list: list of estimates of M_K_B for each AMP iteration
     """
+    # get dimensions and validate Y
     n, p = X.shape
     L = B_hat_k.shape[1]
     assert (Y.shape == (n, 1))
+    # initialise lists to store estimates
     B_hat_list = [B_hat_k.copy()]
     M_k_B_list = []
     # initialise R_hat_minus_1, F_0, Sigma_0
     R_hat_minus_1 = np.zeros((n, L))
     F_k = np.eye(L)
-    # TODO: make this work if the mean is not zero and
-    # if B and B_hat_0 have different distributions
-    Sigma_k = p * np.block([[B_row_cov, np.zeros((L, L))],
-                           [np.zeros((L, L)), B_row_cov]]) / n
+    Sigma_k = p * np.block([[B_row_cov, np.zeros((L, L))], [np.zeros((L, L)), B_row_cov]]) / n
 
     # begin AMP iterations
-    for _ in tqdm(range(n_iters), disable=True):
-        # print(Sigma_k)
+    for _ in range(n_iters):
+        # split Sigma_k into components
         S_1, S_2 = np.hsplit(Sigma_k, 2)
         S_11, S_21 = np.vsplit(S_1, 2)
         S_12, S_22 = np.vsplit(S_2, 2)
-        # step 1:
+        # step 1: compute Theta_k
         Theta_k = X @ B_hat_k - R_hat_minus_1 @ F_k.T
-        # step 2:
+        # step 2: compute R_hat_k using denoising function gk and check for nan
         R_hat_k = apply_gk(Theta_k, Y, S_11, S_12, S_21, S_22, L, sigma_sq, alpha)
         if np.isnan(R_hat_k).any():
             print(f"nan in R_hat_k at iteration {_}, stopping.")
             break
-        # step 3:
+        # step 3: compute C_k
         C_k = compute_Ck(Theta_k, R_hat_k, S_21, S_22, n)
-        # step 4:
+        # step 4: compute B_k estimate of B
         B_k_plus_1 = X.T @ R_hat_k - B_hat_k @ C_k.T
-        # step 5:
+        # step 5: compute M_B_kand B_hat_k using denoising function fk
         M_B_k_plus_1 = R_hat_k.T @ R_hat_k / n
         B_hat_k_plus_1 = apply_fk(B_k_plus_1, M_B_k_plus_1, B_row_cov)
-
-        # step 6:
+        # step 6: compute F_k_plus_1
         F_k_plus_1 = compute_Fk(M_B_k_plus_1, B_row_cov, p, n)
-        # step 7:
+        # step 7: compute updated Sigma_k
         Sigma_k_plus_1 = update_Sigmak(B_hat_k_plus_1, B_row_cov, p, n)
-
-        # prepare next iteration:
+        # store estimates
         B_hat_list.append(B_hat_k_plus_1.copy())
         M_k_B_list.append(M_B_k_plus_1)
-
-        # early stopping
+        # check convergence criterion
         if norm(B_hat_k - B_hat_k_plus_1) ** 2 / p < eps:
-            # print(f"Convergence criterion met, stopping at iteration {_}")
+            print(f"Convergence criterion met at iteration {_}")
             break
-
+        # update variables for next iteration
         B_hat_k = B_hat_k_plus_1
         Sigma_k = Sigma_k_plus_1
         R_hat_minus_1 = R_hat_k
@@ -77,21 +80,24 @@ def matrix_GAMP(X, Y, B_hat_k, B_row_cov, sigma_sq, alpha, n_iters, apply_gk, ep
 
 
 def compute_Ck(Theta_k, R_hat_k, S_21, S_22, n):
+    """ Approximate C_k = g_k'(Theta_k, Ybar) / n """
     return (pinv(S_22) @ (Theta_k.T @ R_hat_k - S_21 @ R_hat_k.T @ R_hat_k) / n).T
 
 
 def compute_Fk(M_B_k, B_cov, p, n):
+    """ Approximate F_k = f_k'(B) / n """
     return p * pinv(M_B_k @ B_cov @ M_B_k.T + M_B_k) @ M_B_k @ B_cov / n
 
 
 def apply_fk(B_k, M_B_k, B_cov):
+    """ Apply denoising function f_k to B_k, where we assume a zero mean gaussian prior with covariance B_cov """
     result = pinv(M_B_k @ B_cov @ M_B_k.T + M_B_k)
     result = B_k @ (B_cov @ M_B_k.T @ result).T
     return result
 
 
 def update_Sigmak(B_hat_k, B_cov, p, n):
+    """ Update state evolution Sigma_k using B_hat_k and B_cov """
     B_hat_cov = B_hat_k.T @ B_hat_k / p
-    tmp = np.block([[B_cov, B_hat_cov],
-                    [B_hat_cov, B_hat_cov]])
+    tmp = np.block([[B_cov, B_hat_cov], [B_hat_cov, B_hat_cov]])
     return p * tmp / n
